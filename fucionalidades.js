8fce57c393e02d93062e3c2119bb24786c5c1dd3
Ezequiel Santos <136353358+ezzysant@users.noreply.github.com>
2025-09-16 16:07:26 -0300

    Funcionalidade do calculo finalizado por enquanto

--- fucionalidades.js
+++ fucionalidades.js
@@ -37,8 +37,26 @@ function amountRow(){
 
 //Função para calculo 01
 function Calculate(){
-    let main_value = document.querySelector('.surebet-entrada');
+    let entrada_value = Number(document.getElementById('surebet-entrada').value);
     let comission = document.querySelectorAll('.comission');
     let odd = document.querySelectorAll('.odd');
     let apostar = document.querySelectorAll('.apostar');
+    let lucro_bruto = document.querySelectorAll('.lucro-bruto')
+
+    //Sei lá, acho que fiz bruxaria pra funcionar
+    for(let i = 0; i < 7; i++){
+        let comission_num = Number(comission[i].value) / 100;
+        let odd_num = Number(odd[i].value);
+
+        let form_calc = ((100 * odd_num - 100) + (100 * odd_num - 100) * comission_num + 100) / 100;
+
+        if(odd_num === 0){return}
+
+        i === 0 ? apostar[0].innerHTML = (entrada_value).toFixed(2) :  apostar[i].innerHTML=(Number(lucro_bruto[0].innerHTML) / form_calc).toFixed(2)
+
+        let apostar_num = Number(apostar[i].innerHTML);
+
+        lucro_bruto[i].innerHTML = Math.round(apostar_num * form_calc).toFixed(2)
+
+    }
 }
